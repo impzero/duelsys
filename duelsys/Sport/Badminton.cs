47f9e3c38a74da248cd8b4d4bcf61b3f82cbe290
Lyubo Lyubchev <l.lyubchev@student.fontys.nl>
2022-06-09 00:31:24 +0200

    wip: backup cause leaving

--- duelsys/Sport/Badminton.cs
+++ duelsys/Sport/Badminton.cs
@@ -47,7 +47,6 @@ namespace duelsys
                 if (result.Value >= score)
                 {
                     winner = result.Key;
-                    score = result.Value;
                 }
             }
 
