73d01b17aa86eaa7ab09a017c081665bf7f9e342
Lyubo Lyubchev <l.lyubchev@student.fontys.nl>
2022-06-09 15:30:40 +0200

    feat: configure web Program.cs

--- web/Program.cs
+++ web/Program.cs
@@ -1,7 +1,42 @@
+using duelsys.ApplicationLayer.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using mysql;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(
+    options =>
+    {
+        options.LoginPath = new PathString("/auth/SignIn");
+        options.AccessDeniedPath = new PathString("/auth/SignIn");
+    });
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+
+string connectionUrl = "Server=localhost;Uid=root;Database=duelsys;Pwd=123456";
+
+var userStore = new UserStore(connectionUrl);
+var tournamentStore = new TournamentStore(connectionUrl);
+var matchStore = new MatchStore(connectionUrl);
+var gameStore = new GameStore(connectionUrl);
+var sportStore = new SportStore(connectionUrl);
+var tournamentSystemStore = new TournamentSystemStore(connectionUrl);
+
+var aService = new AuthenticationService(userStore);
+var tService = new TournamentService(tournamentStore, matchStore, gameStore);
+var tsService = new TournamentSystemService(tournamentSystemStore);
+var sService = new SportService(sportStore);
+var uService = new UserService(userStore);
+var mService = new MatchService(matchStore);
+
+builder.Services.AddSingleton(aService);
+builder.Services.AddSingleton(tService);
+builder.Services.AddSingleton(tsService);
+builder.Services.AddSingleton(sService);
+builder.Services.AddSingleton(uService);
+builder.Services.AddSingleton(mService);
 
 var app = builder.Build();
 
@@ -18,6 +53,7 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
