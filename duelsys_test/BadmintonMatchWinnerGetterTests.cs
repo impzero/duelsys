168d59a7fbe2b74dfe8b0280c2bae785a7a418f4
Lyubo Lyubchev <l.lyubchev@student.fontys.nl>
2022-06-10 11:52:33 +0200

    fix: tests

--- duelsys_test/BadmintonMatchWinnerGetterTests.cs
+++ duelsys_test/BadmintonMatchWinnerGetterTests.cs
@@ -1,4 +1,5 @@
 ﻿using duelsys;
+using duelsys.Exceptions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
@@ -33,5 +34,29 @@ public class BadmintonMatchWinnerGetterTests
 
         var thirdWinner = getter.DecideWinner(g6, g5);
         Assert.AreEqual(secondPlayerId, thirdWinner.Id);
+    }
+
+    [TestMethod]
+    public void DecideWinner_ThrowsError()
+    {
+        var getter = new BadmintonGame.BadmintonMatchWinnerGetter();
+
+        var firstPlayerId = 123;
+        var secondPlayerId = 13;
+        var u1 = new UserBase(firstPlayerId, "lyubo", "lyubchev");
+        var u2 = new UserBase(secondPlayerId, "alex", "alex");
+
+        var g1 = new List<Game> { new(u1, "21"), new(u1, "25") };
+        var g2 = new List<Game> { new(u2, "4"), new(u2, "28"), new(u2, "23") };
+
+        var g3 = new List<Game> { new(u1, "4"), new(u1, "30") };
+        var g4 = new List<Game> { new(u2, "21"), new(u2, "28"), new(u2, "23") };
+
+        var g5 = new List<Game> { new(u1, "4"), new(u1, "23") };
+        var g6 = new List<Game> { new(u2, "21"), new(u2, "25") };
+
+        Assert.ThrowsException<InvalidMatchException>(() => getter.DecideWinner(g2, g1));
+        Assert.ThrowsException<InvalidMatchException>(() => getter.DecideWinner(g4, g3));
+        Assert.ThrowsException<InvalidMatchException>(() => getter.DecideWinner(g6, g5));
     }
 }
